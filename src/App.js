d37260fe6e63e92a1ede8362306cc8de4b6f2438
Krushinsky <77190162+Krushinsky@users.noreply.github.com>
2022-10-09 10:14:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 useEffect ( ()=>{
   async function newRecipe(){
   const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=${MY_ID}&app_key=${MY_KEY}`)
-const data = await response .json()
+const data = await response.json()
 console.log(data.hits)
 setMyRecipes(data.hits)
   }
